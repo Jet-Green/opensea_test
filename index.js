3d61ff196a7cc76cda72a1d970099d9d3a7cc1a3
Jet-Green <grishadzyin@gmail.com>
2022-01-22 16:10:16 +0400

    add reciving nft by id

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const sdk = require('api')('@opensea/v1.0#1j3wv35kyd6wqwc');
 /***
  * Если приходит ошибка, то нужно подождать и ещё раз сделать запрос(не знаю, почему)
  */
-sdk['retrieving-assets-rinkeby']({ owner: "0x17d27868334fa69668f44c8e1a25d40f4c742cf7", order_direction: 'desc', offset: '0', limit: '3' })
+sdk['retrieving-assets-rinkeby'](
+    {
+        owner: '0x062d6d315e6c8aa196b9072d749e3f3f3579fdd0',
+        token_ids: '20',
+        order_direction: 'desc',
+        offset: '0',
+        // limit 1, потому что встречаются nft с одинаковыми id
+        limit: '1'
+    })
     .then(res => console.log(res))
     .catch(err => {
         console.error(err.status, err.statusText)
